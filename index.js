ec8adfe58fa492b442b4bae8f025a62ac2fca5da
jesse.langat@student.moringaschool.com <jesse.langat@student.moringaschool.com>
2024-04-02 13:41:43 +0300

    Users/macbook/Developments/arithmetic/phase-1-arithmetic-lab/.git

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 20;
+let num4 = 4;
+let multiply = num1 * num2;
+
+let random_number = Math.floor(Math.random() * 100) + 1;
+
+let mod = num3 % num4;
+
+let numbers_set = [5, 12, 20, 7, 14];
+let max_number = Math.max(...numbers_set);
+
+console.log("multiply:", multiply);
+console.log("random number:", random_number);
+console.log("mod:", mod);
+console.log("max number:", max_number);
